befe166eb5490d8d2d371561619f2418cfb43614
phseon <hye4659@gmail.com>
2021-11-03 11:33:56 +0900

    bookintro detail

--- src/container/InfoPage/BookIntro.js
+++ src/container/InfoPage/BookIntro.js
@@ -3,20 +3,25 @@ const BookIntro = () => {
 
 
 return (
+    <>
+    <div className="BookDepart">
+    <h3>학과 이름 | 중고 여부</h3>
     <div className="BookIntro">
-    <p>학과 이름 | 중고 여부</p>
-    <p>도서명</p>
-    <p>가격</p>
-    <p>등록일자</p>
-    <p>중고 상태</p>
-
-
+    <h2>도서명</h2>
+    <h2>가격</h2>
     <div className="itemSelect">
-    <label className="inputFileButton" for="file">
+    {/* <label className="inputFileButton" for="file">
     사진 선택
-    </label>
+    </label> */}
     </div>
     </div>
+    </div>
+    <div className="Bookstate">
+    <h3>등록일자</h3>
+    <h3>중고 상태</h3>
+    </div>
+    </>
     );
+    //사진선택빼기 - 등록페이지로
 };
 export default BookIntro;
